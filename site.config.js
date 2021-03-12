dd4b78bfeed01623aa8d3f34d4a2843afe86eb2f
rotterlt <rotterlt@outlook.com>
2021-03-13 00:06:47 +0800

    1

---
+++ site.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '42c99f2c4d5c4a81a056c5972a1fefeb',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Rotter & Hannah',
+  domain: 'ronah.xcxr.site',
+  author: 'RotterL',
+
+  // open graph metadata (optional)
+  // description: '基础医学实验技术学习',
+  // socialImageTitle: 'Rotterl',
+  // socialImageSubtitle: 'Hello World! 👋',
+
+  // social usernames (optional)
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+   utterancesGitHubRepo: null ,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/biotech': 'f0e89f6361344d9f88032bbef1abd7b9'
+  // }
+  pageUrlOverrides: false
+}
